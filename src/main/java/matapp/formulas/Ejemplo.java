9c7259d7d59d05780b87abfed2af0f50f725d827
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2020-01-31 14:13:52 +0000

    ejemplo de uso de FXGson

--- src/main/java/matapp/formulas/Ejemplo.java
+++ src/main/java/matapp/formulas/Ejemplo.java
@@ -5,48 +5,55 @@ import com.google.gson.GsonBuilder;
 
 import static matapp.expreval.ExprEval.*;
 
+import org.hildan.fxgson.FxGson;
+import org.hildan.fxgson.FxGsonBuilder;
+
 public class Ejemplo {
 
 	public static void main(String[] args) {
 
-		Variable v = new Variable();
-		v.setName("v");
-		v.setDescripcion("velocidad");
+//		Variable v = new Variable();
+//		v.setName("v");
+//		v.setDescripcion("velocidad");
+//
+//		Variable d = new Variable();
+//		d.setName("d");
+//		d.setDescripcion("distancia");
+//
+//		Variable t = new Variable();
+//		t.setName("t");
+//		t.setDescripcion("tiempo");
+//
+//		Formula f = new Formula();
+//		f.setName("Velocidad");
+//		f.setDescription("Rapidez en la que viaja un objeto en una dirección específica");
+//		f.setExpression("d/t");
+//		f.setResult(v);
+//		f.getVariables().add(d);
+//		f.getVariables().add(t);
+//
+//		Category c = new Category();
+//		c.setName("Cinemática");
+//		c.getFormulas().add(f);
+//
+//		FormulaList fl = new FormulaList();
+//		fl.getCategories().add(c);
 
-		Variable d = new Variable();
-		d.setName("d");
-		d.setDescripcion("distancia");
-
-		Variable t = new Variable();
-		t.setName("t");
-		t.setDescripcion("tiempo");
-
-		Formula f = new Formula();
-		f.setName("Velocidad");
-		f.setDescription("Rapidez en la que viaja un objeto en una dirección específica");
-		f.setExpression("d/t");
-		f.setResult(v);
-		f.getVariables().add(d);
-		f.getVariables().add(t);
-
-		Category c = new Category();
-		c.setName("Cinemática");
-		c.getFormulas().add(f);
-
-		FormulaList fl = new FormulaList();
-		fl.getCategories().add(c);
-
+		Variable a = new Variable();
+		a.setName("Nombre");
+		a.setDescripcion("No se qué");
+		
 		// muestra la lista de fórmulas en json
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		System.out.println(gson.toJson(fl) + "\n");
+		Gson gson = FxGson.fullBuilder().setPrettyPrinting().create();
+		System.out.println(gson.toJson(a) + "\n");
 
 		// evalúa la expresión de la formula con unos valores
-		Double r = (Double) eval(
-				f.getExpression(), 
-				param(d.getName(), 15), 
-				param(t.getName(), 6)
-			);
-		System.out.println(v.getName() + "=" + r);
+//		Double r = (Double) eval(
+//				f.getExpression(), 
+//				param(d.getName(), 15), 
+//				param(t.getName(), 6)
+//			);
+//		System.out.println(v.getName() + "=" + r);
 		
 	}
 
